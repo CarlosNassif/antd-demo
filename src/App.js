e80c987f0c30bfc7dc973a7675903c031749e98f
Carlos Nassif <costa.carlos12@gmail.com>
2023-03-21 23:07:55 -0300

    add theme

--- src/App.js
+++ src/App.js
@@ -1,24 +1,48 @@
-import logo from './logo.svg';
 import './App.css';
+
+import {
+  Button,
+  DatePicker,
+  Input,
+  Progress,
+  Slider,
+  Space,
+  Spin,
+  ConfigProvider,
+} from 'antd';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ConfigProvider
+      theme={{
+        token: {
+          colorPrimary: '#00b96b',
+          colorBorder: '#09f',
+          colorText: '#0f9',
+        },
+      }}
+    >
+      <div className='App'>
+        <header className='App-header'>
+          <Space direction='vertical'>
+            <Button
+              className='button'
+              style={{
+                color: 'yellow',
+              }}
+              type='primary'
+            >
+              Button
+            </Button>
+            <Input placeholder='Type here...'></Input>
+            <Progress percent={50} type='circle' />
+            <Spin spinning />
+            <DatePicker />
+            <Slider />
+          </Space>
+        </header>
+      </div>
+    </ConfigProvider>
   );
 }
 
